b1620232662da2a4660af94d23a9d5471d3580c4
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-11 15:26:07 +0100

    Script ultimato - Esercizio finito.
    Aggiunto un commento che "spiega" l'esercizio.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+/*Sono stati creati due eventi: uno per il click del bottone hamburger (che apre il menu) e uno per il click del bottone a croce (che chiude il menu).
+Al loro click, ho impostato un fade-in e un fade-out a 200 ms.
+Per selezionare i bottoni nell html ho usato il selettore > visto che erano gli unici link figli diretti (non contenuti in una <ul>) de rispettivi menu.*/
+
+
 var openButton = $(".header-right > a");
 var closeButton = $(".hamburger-menu > a");
 var hamburgerMenu = $(".hamburger-menu");
